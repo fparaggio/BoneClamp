24bbfb2cc13341d14fab455634417c39c15eadea
yapatel525 <yapatel@gatech.edu>
2013-04-06 16:23:42 -0400

    adding hdf tools

--- xi-bone/setup.sh
+++ xi-bone/setup.sh
@@ -5,8 +5,8 @@
 
 #-------------------------------------------------------------------------
 
-#	Filename: setup.sh simplifies the installation process by automatically checking
-#             for dependencies and installing xi-bone
+#	Filename: setup.sh simplifies the installation process by automatically
+#             installing dependencies and xi-bone
 
 #	Written in 2013 by:
 #		Jon Newman <jnewman6@gatech.edu>
@@ -29,14 +29,19 @@ sudo apt-get upgrade
 sudo apt-get install automake libtool autoconf autotools-dev build-essential qt3-dev-tools libboost-dev libboost-program-options-dev libgsl0-dev bison flex libncurses5-dev
 
 # Installing HDF5
-echo "Installing HDF5...\n"
+echo "Installing HDF5..."
+
+tar xf hdf/hdf5-1.8.4.tar.bz2 -C hdf/.
+./hdf/hdf5-1.8.4/configure
+make -C hdf/hdf5-1.8.4/
+sudo make install -C hdf5/hdf5-1.8.4/
 
 # Start configuring - by default configured to run on non-RT kernel
-echo "Starting xi-bone installation...\n"
+echo "Starting xi-bone installation..."
 
 ./autogen.sh
 ./configure --disable-comedi --disable-rtai --disable-xenomai --disable-analogy --disable-ni
-make
-sudo make install
+make -C ./
+sudo make install -C ./
 
-echo "Installation complete...Happy Sciencing!\n"
+echo "Installation complete...Happy Sciencing!"
